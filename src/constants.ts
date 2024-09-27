39a13a2ff23bbca6810a7c3c71ecff73d3794ec9
Sergey-Markov <1nativefl@gmail.com>
2024-09-27 12:12:42 +0300

    add MENU_ITEMS

--- src/constants.ts
+++ src/constants.ts
@@ -1,22 +1,59 @@
-import { v4 as uuidv4 } from "uuid";
+import { v4 as uuid } from "uuid";
+import { IoLanguage, IoWallet } from "react-icons/io5";
+import { BsFillInfoCircleFill } from "react-icons/bs";
+import { LuCalendarHeart } from "react-icons/lu";
+import { FaHandsHelping } from "react-icons/fa";
 
 export const formFirstVoteOptions = [
   {
-    id: uuidv4(),
+    id: uuid(),
     name: "futureGrinG",
     valueField: "leprechaun",
     checked: false,
   },
   {
-    id: uuidv4(),
+    id: uuid(),
     name: "futureGrinG",
     valueField: "investor",
     checked: false,
   },
   {
-    id: uuidv4(),
+    id: uuid(),
     name: "futureGrinG",
     valueField: "pirate",
     checked: true,
+  },
+];
+
+export const MENU_ITEMS = [
+  {
+    id: uuid(),
+    label: "wallet",
+    href: "/",
+    icon: IoWallet,
+  },
+  {
+    id: uuid(),
+    label: "language",
+    href: "/",
+    icon: IoLanguage,
+  },
+  {
+    id: uuid(),
+    label: "about",
+    href: "/",
+    icon: BsFillInfoCircleFill,
+  },
+  {
+    id: uuid(),
+    label: "road map",
+    href: "/",
+    icon: LuCalendarHeart,
+  },
+  {
+    id: uuid(),
+    label: "support",
+    href: "/",
+    icon: FaHandsHelping,
   },
 ];
