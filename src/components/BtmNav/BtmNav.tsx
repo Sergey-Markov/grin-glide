de8a55f138acdeadb6887ad29aaf24be62feda73
Sergey-Markov <1nativefl@gmail.com>
2024-09-30 14:32:35 +0300

    add rout Game

--- src/components/BtmNav/BtmNav.tsx
+++ src/components/BtmNav/BtmNav.tsx
@@ -1,41 +1,10 @@
 "use client";
 
-import { v4 as uuidv4 } from "uuid";
+import { memo } from "react";
 import classNames from "classnames";
-import { IoHomeOutline } from "react-icons/io5";
-import { BsFillBarChartLineFill } from "react-icons/bs";
-import { GoTasklist } from "react-icons/go";
-import { GiThreeFriends } from "react-icons/gi";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { memo } from "react";
-
-const btmNavigationArr = [
-  {
-    id: uuidv4(),
-    label: "Home",
-    icon: IoHomeOutline,
-    href: "/",
-  },
-  {
-    id: uuidv4(),
-    label: "Liderboard",
-    icon: BsFillBarChartLineFill,
-    href: "/liderboard",
-  },
-  {
-    id: uuidv4(),
-    label: "Tasks",
-    icon: GoTasklist,
-    href: "/tasks",
-  },
-  {
-    id: uuidv4(),
-    label: "Friends",
-    icon: GiThreeFriends,
-    href: "/friends",
-  },
-];
+import { BOTTOM_NAVIGATION } from "@/constants";
 
 const BtmNav = memo(() => {
   const pathName = usePathname();
@@ -43,7 +12,7 @@ const BtmNav = memo(() => {
   // const t = useTranslations("HomePage");
   return (
     <nav className="btm-nav text-primary overflow-hidden mx-auto my-0 bg-emerald-900 backdrop-blur-md rounded-full xs:btm-nav-xs sm:btm-nav-sm md:btm-nav-md bottom-4 max-w-screen-md">
-      {btmNavigationArr.map(({ id, label, icon: Icon, href }) => (
+      {BOTTOM_NAVIGATION.map(({ id, label, icon: Icon, href }) => (
         <button
           key={id}
           type="button"
