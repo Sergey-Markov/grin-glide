70f6aa1971ada15c094c412ed2e56d0f4772fb9f
Sergey-Markov <1nativefl@gmail.com>
2024-10-25 11:46:25 +0300

    try check a route to DB 4

--- src/app/api/getDB/route.ts
+++ src/app/api/getDB/route.ts
@@ -1,16 +1,13 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
 // import { NextRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
 import { connectToDatabase } from "@/lib/mongodb";
-import { NextRequest, NextResponse } from "next/server";
 
-export default async function handler(req: NextRequest, res: NextResponse) {
+export default async function handler(req: Request) {
   try {
     const client = await connectToDatabase();
     const db = client.db("sample_mflix");
-    if (!db) {
-      NextResponse.json("db not supported 111", { status: 222 });
-    }
 
     const collections = await db.listCollections().toArray();
     return NextResponse.json({ collections });
