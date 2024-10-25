63ee676f879ec559b29b0d0930fd100ac0f73205
Sergey-Markov <1nativefl@gmail.com>
2024-10-25 12:07:02 +0300

    try check a route to DB 5

--- src/app/api/getDB/route.ts
+++ src/app/api/getDB/route.ts
@@ -7,10 +7,10 @@ import { connectToDatabase } from "@/lib/mongodb";
 export const GET = async (req: Request) => {
   try {
     const client = await connectToDatabase();
-    const db = client.db("users");
-
-    const collections = await db.listCollections().toArray();
-    return NextResponse.json({ collections });
+    const db = client.db("sample_mflix");
+    const data = db.databaseName;
+    // const collections = await db.listCollections().toArray();
+    return NextResponse.json({ data });
   } catch (error) {
     console.error("An error occurred while fetching data from MongoDB:", error);
     return new NextResponse("Internal Server Error", { status: 500 });
