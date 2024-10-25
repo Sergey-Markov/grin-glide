0c067ce563ff1050e6243793dec15f4682fe4930
Sergey-Markov <1nativefl@gmail.com>
2024-10-25 11:01:33 +0300

    check a route to DB

--- src/app/api/getDB/route.ts
+++ src/app/api/getDB/route.ts
@@ -1,10 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { connectToDatabase } from "@/lib/mongodb";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export const GET = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "GET") {
     try {
       const client = await connectToDatabase();
@@ -19,4 +16,4 @@ export default async function handler(
     res.setHeader("Allow", ["GET"]);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
-}
+};
