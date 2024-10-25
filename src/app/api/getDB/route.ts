f5e1e16d5d8ea63303faa5386eb5a2584b5a7a45
Sergey-Markov <1nativefl@gmail.com>
2024-10-25 11:19:35 +0300

    try check a route to DB 1

--- src/app/api/getDB/route.ts
+++ src/app/api/getDB/route.ts
@@ -2,21 +2,17 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { connectToDatabase } from "@/lib/mongodb";
 
-export const GET = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === "GET") {
-    try {
-      console.log("start");
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const client = await connectToDatabase();
+    const db = client.db("telegram-miniapp");
 
-      const client = await connectToDatabase();
-      const db = client.db("sample_mflix");
-
-      const data = await db.collection("users").find({}).toArray();
-      res.status(200).json(data);
-    } catch (error) {
-      res.status(500).json({ error: "Failed to fetch data." });
-    }
-  } else {
-    res.setHeader("Allow", ["GET"]);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+    const collections = await db.listCollections().toArray();
+    res.status(200).json({ collections });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error", error });
   }
-};
+}
