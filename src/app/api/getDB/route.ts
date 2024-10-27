05b95369cada16cdcff9a752a61b45cd0ca8bd23
Sergey-Markov <1nativefl@gmail.com>
2024-10-27 18:41:07 +0200

    try check a route to DB 21

--- src/app/api/getDB/route.ts
+++ src/app/api/getDB/route.ts
@@ -7,10 +7,10 @@ import { connectToDatabase } from "@/lib/mongodb";
 export const GET = async (req: Request) => {
   try {
     const client = await connectToDatabase();
-    const db = client.db("users");
+    const db = client.db("sample_mflix");
 
-    // const collections = await db.listCollections().toArray();
-    return NextResponse.json({ db });
+    const collections = await db.listCollections().toArray();
+    return NextResponse.json({ collections });
   } catch (error) {
     console.error("An error occurred while fetching data from MongoDB:", error);
     return new NextResponse(`${error}`, { status: 500 });
