a05703cac2d4b049b4bf81d8b9a0c9f21032114f
Sergey-Markov <1nativefl@gmail.com>
2024-10-27 18:54:25 +0200

    try check a route to DB 22/1

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -16,20 +16,20 @@ const client = new MongoClient(uri, {
   serverSelectionTimeoutMS: 5000,
 });
 
-async function run() {
-  try {
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+// async function run() {
+//   try {
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log(
+//       "Pinged your deployment. You successfully connected to MongoDB!"
+//     );
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
 
 export async function connectToDatabase() {
   try {
