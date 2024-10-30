5c3c20dbacb8c7d1797de7c8ba9f9b6584d54818
Sergey-Markov <1nativefl@gmail.com>
2024-10-30 10:40:28 +0200

    fix lang 1

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 import { NextIntlClientProvider } from "next-intl";
-import { getMessages } from "next-intl/server";
+import { inter, merriweather } from "@/fonts";
+import { getLocale, getMessages } from "next-intl/server";
 import BtmNav from "@/components/BtmNav/BtmNav";
 
 export default async function RootLayout({
@@ -9,14 +10,20 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const locale = await getLocale();
   const messages = await getMessages();
 
   return (
-    <div>
-      <NextIntlClientProvider messages={messages}>
-        {children}
-        <BtmNav />
-      </NextIntlClientProvider>
-    </div>
+    <html
+      data-theme="cupcake"
+      lang={locale}
+    >
+      <body className={`${inter.variable} ${merriweather.variable}`}>
+        <NextIntlClientProvider messages={messages}>
+          {children}
+          <BtmNav />
+        </NextIntlClientProvider>
+      </body>
+    </html>
   );
 }
