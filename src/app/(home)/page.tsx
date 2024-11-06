83c727b7703b0326a47195203cd237cd567b2a65
Sergey-Markov <1nativefl@gmail.com>
2024-11-06 16:49:36 +0200

    add preloader in dynamic component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,54 +1,12 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import VoteForm from "@/components/VoteForm/VoteForm";
-import { formFirstVoteOptions } from "@/constants";
-import Menu from "@/components/Menu/Menu";
-import MainHeader from "@/components/MainHeader/MainHeader";
-import HeroStartTask from "@/components/HeroStartTask/HeroStartTask";
-import WebApp from "@twa-dev/sdk";
+// import HomePage from "@/components/Home/Home";
 import Preloader from "@/components/Preloader/Preloader";
-import { IDbUser, useTelegramUser } from "@/hooks/useTelegramUser";
+import dynamic from "next/dynamic";
+
+const DynamicHomePage = dynamic(() => import("@/components/Home/Home"), {
+  ssr: false,
+  loading: () => <Preloader />,
+});
 
 export default function Home() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [user, setUser] = useState<IDbUser | null>(null);
-  const { userTelegram } = useTelegramUser();
-
-  useEffect(() => {
-    if (WebApp && userTelegram) {
-      if (!user) {
-        setUser(userTelegram);
-      }
-    }
-  }, [setUser, user, userTelegram]);
-
-  const openMenuHandler = () => {
-    setIsOpen(!isOpen);
-  };
-
-  if (!user) {
-    return <Preloader />;
-  }
-
-  return (
-    <div className="font-sans text-white min-h-screen pb-12">
-      <div className="relative mx-auto px-3 py-6 overflow-hidden">
-        <MainHeader
-          open={isOpen}
-          openToggler={openMenuHandler}
-        />
-        <Menu open={isOpen} />
-        <main className="p-4">
-          <section
-            id="hero-home"
-            className="flex justify-center items-center mb-8"
-          >
-            <HeroStartTask />
-          </section>
-          <VoteForm options={formFirstVoteOptions} />
-        </main>
-      </div>
-    </div>
-  );
+  return <DynamicHomePage />;
 }
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import VoteForm from "@/components/VoteForm/VoteForm";
+import { formFirstVoteOptions } from "@/constants";
+import Menu from "@/components/Menu/Menu";
+import MainHeader from "@/components/MainHeader/MainHeader";
+import HeroStartTask from "@/components/HeroStartTask/HeroStartTask";
+import WebApp from "@twa-dev/sdk";
+import { IDbUser, useTelegramUser } from "@/hooks/useTelegramUser";
+
+const HomePage = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [user, setUser] = useState<IDbUser | null>(null);
+  const { userTelegram } = useTelegramUser();
+
+  useEffect(() => {
+    if (WebApp && userTelegram) {
+      if (!user) {
+        setUser(userTelegram);
+      }
+    }
+  }, [setUser, user, userTelegram]);
+
+  const openMenuHandler = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="font-sans text-white min-h-screen pb-12">
+      <div className="relative mx-auto px-3 py-6 overflow-hidden">
+        <MainHeader
+          open={isOpen}
+          openToggler={openMenuHandler}
+        />
+        <Menu open={isOpen} />
+        <main className="p-4">
+          <section
+            id="hero-home"
+            className="flex justify-center items-center mb-8"
+          >
+            <HeroStartTask />
+          </section>
+          <VoteForm options={formFirstVoteOptions} />
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
