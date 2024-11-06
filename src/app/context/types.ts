3059cf65a462dc840563f72e5b8f8c4b1e68fa60
Sergey-Markov <73185530+Sergey-Markov@users.noreply.github.com>
2024-11-06 16:03:59 +0200

    Delete src/app/context directory

--- src/app/context/types.ts
+++
@@ -1,7 +0,0 @@
-import { IDbUser } from "@/hooks/useTelegramUser";
-import React from "react";
-
-export interface UserContextType {
-  user: IDbUser | null;
-  setUser: React.Dispatch<React.SetStateAction<IDbUser | null>>;
-}
