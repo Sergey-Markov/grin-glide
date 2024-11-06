8f6bfd5c952b06041fd919aa88715789344100f4
Sergey-Markov <1nativefl@gmail.com>
2024-11-06 15:08:03 +0200

    Revert "fix start update 5"
    This reverts commit 0eebcf6e8a68af66eaa4a334e10b2dca9ffc5380.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,32 @@ import Preloader from "@/components/Preloader/Preloader";
 // import { useUser } from "./context/UserContext";
 
 export default function Home() {
+<<<<<<< HEAD
   if (WebApp) {
     redirect(`/main`);
+=======
+  const [isOpen, setIsOpen] = useState(false);
+  const [tgUserData, setTgUserData] = useState<IDbUser | null>(null);
+  const { user } = useTelegramUser();
+
+  useEffect(() => {
+    if (WebApp && user) {
+      if (!tgUserData) {
+        setTgUserData(user);
+        WebApp.showAlert("Hello bro");
+      }
+    }
+  }, [user]);
+
+  console.log("user:", tgUserData);
+
+  const openMenuHandler = () => {
+    setIsOpen(!isOpen);
+  };
+
+  if (!tgUserData) {
+    return <Preloader />;
+>>>>>>> parent of 0eebcf6 (fix start update 5)
   }
 
   return <Preloader />;
