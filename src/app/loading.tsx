9a42b2a75ceb7657d28149cdfbdc2b89d1f79855
Sergey-Markov <1nativefl@gmail.com>
2024-11-06 17:43:48 +0200

    add styles

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,9 @@
 import Preloader from "@/components/Preloader/Preloader";
 
-const LoadingPage = () => <Preloader />;
+const LoadingPage = () => {
+  <div className="absolute top-0 right-0 min-h-screen min-w-full ">
+    <Preloader />
+  </div>;
+};
 
 export default LoadingPage;
