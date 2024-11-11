13954fd23bf9c332d0e80f583828add6a1c92236
Sergey-Markov <1nativefl@gmail.com>
2024-11-11 15:26:33 +0200

    added translations for Toast

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -1,9 +1,17 @@
-const Toast = () => (
-  <div className="toast toast-top toast-center">
-    <div className="alert alert-warning">
-      <span>Failed to copy the link</span>
+"use client";
+
+import { useTranslations } from "next-intl";
+
+const Toast = () => {
+  const t = useTranslations("Toast");
+
+  return (
+    <div className="toast toast-top toast-center">
+      <div className="alert alert-warning">
+        <span>{t("failedCopyLink")}</span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Toast;
