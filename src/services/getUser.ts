07c687f4d45b89b29eca9f90331dad21730ffe22
Sergey-Markov <1nativefl@gmail.com>
2024-11-12 12:47:55 +0200

    try fix fetch user data api & service - 1

--- src/services/getUser.ts
+++ src/services/getUser.ts
@@ -4,7 +4,13 @@ import { TelegramUser } from "@/hooks/useTelegramUser";
 
 export async function getUser(user: TelegramUser) {
   try {
-    const response = await axios.post("/api/get-user-db", user, {
+    const response = await axios.get("/api/get-user-db", {
+      params: {
+        telegram_id: user.id,
+        username: user.username,
+        first_name: user.first_name,
+        last_name: user.last_name,
+      },
       headers: {
         "Content-Type": "application/json",
       },
