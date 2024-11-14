603790083e32845c9a14c5946e77b339aa0bc0a8
Sergey-Markov <1nativefl@gmail.com>
2024-11-14 16:30:38 +0200

    try add Modal to home add z-50

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,30 +1,53 @@
-import { ReactNode } from "react";
+"use client";
+
+import { ReactNode, useEffect } from "react";
 
 interface ModalProps {
   closeModal: () => void;
   children: ReactNode;
 }
 
-const Modal = ({ closeModal, children }: ModalProps) => (
-  <dialog
-    id="my_modal_5"
-    className="modal modal-bottom sm:modal-middle bg-black z-50"
-  >
-    <div className="modal-box">
-      {children}
-      <div className="modal-action">
-        <form method="dialog">
+const Modal = ({ closeModal, children }: ModalProps) => {
+  const openModalHandler = () => {
+    const modal = document?.getElementById("my_modal_5") as HTMLDialogElement;
+
+    if (modal?.showModal) {
+      modal.showModal();
+    } else {
+      // eslint-disable-next-line no-console
+      console.error("Dialog element or showModal method is not supported.");
+    }
+  };
+
+  useEffect(() => {
+    openModalHandler();
+  }, []);
+
+  return (
+    <dialog
+      id="my_modal_5"
+      className="modal modal-bottom sm:modal-middle bg-black z-50"
+    >
+      <div className="modal-box">
+        {children}
+        <div className="modal-action">
           <button
             type="button"
             className="btn"
-            onClick={closeModal}
+            onClick={() => {
+              const modal = document.getElementById(
+                "my_modal_5"
+              ) as HTMLDialogElement;
+              modal?.close();
+              closeModal();
+            }}
           >
             Close
           </button>
-        </form>
+        </div>
       </div>
-    </div>
-  </dialog>
-);
+    </dialog>
+  );
+};
 
 export default Modal;
