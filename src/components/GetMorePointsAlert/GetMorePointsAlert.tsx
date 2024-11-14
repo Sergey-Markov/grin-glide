d4b051a7ae1de1c34dd7c2ef104fc42a54bf619c
Sergey-Markov <1nativefl@gmail.com>
2024-11-14 18:53:00 +0200

    try add confetty

--- src/components/GetMorePointsAlert/GetMorePointsAlert.tsx
+++ src/components/GetMorePointsAlert/GetMorePointsAlert.tsx
@@ -1,21 +1,32 @@
 interface GetMorePointsAlertProps {
   points: number;
   closeModal: () => void;
+  triggerConfetti?: () => void; // Опциональный пропс
 }
 
 const GetMorePointsAlert = ({
   points,
   closeModal,
-}: GetMorePointsAlertProps) => (
-  <div className="flex justify-center items-center w-full h-full">
-    <button
-      type="button"
-      className="btn  btn-success"
-      onClick={closeModal}
-    >
-      <p>{`Тримай бонус ${points}`}</p>
-    </button>
-  </div>
-);
+  triggerConfetti, // Получаем функцию активации конфетти
+}: GetMorePointsAlertProps) => {
+  const handleClick = () => {
+    if (triggerConfetti) {
+      triggerConfetti(); // Активируем конфетти
+    }
+    closeModal(); // Закрываем модалку
+  };
+
+  return (
+    <div className="flex justify-center items-center w-full h-full">
+      <button
+        type="button"
+        className="btn btn-success"
+        onClick={handleClick}
+      >
+        <p>{`Тримай бонус ${points}`}</p>
+      </button>
+    </div>
+  );
+};
 
 export default GetMorePointsAlert;
