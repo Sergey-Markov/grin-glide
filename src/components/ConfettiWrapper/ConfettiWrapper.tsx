b6b701f131198db12045b46f8f03ece579c7603d
Sergey-Markov <1nativefl@gmail.com>
2024-11-14 19:24:48 +0200

    add numberOfPieces from 400 to 500

--- src/components/ConfettiWrapper/ConfettiWrapper.tsx
+++ src/components/ConfettiWrapper/ConfettiWrapper.tsx
@@ -3,19 +3,18 @@
 import React, { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
 
-// Динамічний імпорт для оптимізації завантаження
 const Confetti = dynamic(() => import("react-confetti"), { ssr: false });
 
 interface AutoConfettiWrapperProps {
   children: React.ReactNode;
-  interval?: number; // Інтервал між запусками конфеті (в мілісекундах)
-  duration?: number; // Тривалість показу конфеті (в мілісекундах)
+  interval?: number;
+  duration?: number;
 }
 
 export default function AutoConfettiWrapper({
   children,
-  interval = 8000, // За замовчуванням, запускаємо кожні 10 секунд
-  duration = 5000, // За замовчуванням, показуємо протягом 5 секунд
+  interval = 8000,
+  duration = 5000,
 }: AutoConfettiWrapperProps) {
   const [isConfettiActive, setIsConfettiActive] = useState(false);
   const [windowDimensions, setWindowDimensions] = useState({
@@ -24,13 +23,11 @@ export default function AutoConfettiWrapper({
   });
 
   useEffect(() => {
-    // Встановлюємо початкові розміри вікна
     setWindowDimensions({
       width: window.innerWidth,
       height: window.innerHeight,
     });
 
-    // Оновлюємо розміри при зміні розміру вікна
     const handleResize = () => {
       setWindowDimensions({
         width: window.innerWidth,
@@ -40,19 +37,15 @@ export default function AutoConfettiWrapper({
 
     window.addEventListener("resize", handleResize);
 
-    // Функція для запуску конфеті
     const triggerConfetti = () => {
       setIsConfettiActive(true);
       setTimeout(() => setIsConfettiActive(false), duration);
     };
 
-    // Запускаємо конфеті одразу при монтуванні компонента
     triggerConfetti();
 
-    // Встановлюємо інтервал для періодичного запуску конфеті
     const confettiInterval = setInterval(triggerConfetti, interval);
 
-    // Очищення при розмонтуванні компонента
     return () => {
       window.removeEventListener("resize", handleResize);
       clearInterval(confettiInterval);
@@ -66,7 +59,7 @@ export default function AutoConfettiWrapper({
           width={windowDimensions.width}
           height={windowDimensions.height}
           recycle={false}
-          numberOfPieces={400}
+          numberOfPieces={500}
         />
       )}
       {children}
