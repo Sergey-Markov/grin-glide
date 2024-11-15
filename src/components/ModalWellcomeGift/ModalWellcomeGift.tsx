90a6b57e18ceebe8f6865d518b43a0a59974094f
Sergey-Markov <1nativefl@gmail.com>
2024-11-15 18:28:43 +0200

    change modal inner box corners styles

--- src/components/ModalWellcomeGift/ModalWellcomeGift.tsx
+++ src/components/ModalWellcomeGift/ModalWellcomeGift.tsx
@@ -31,7 +31,7 @@ const ModalWellcomeGift = ({ closeModal }: ModalProps) => {
       className="modal modal-bottom sm:modal-middle flex flex-col justify-end items-center"
     >
       <ConfettiWrapper>
-        <div className=" bg-slate-900 w-full h-1/3 ">
+        <div className=" bg-slate-900 w-full h-1/3 rounded-t-lg ">
           <GetMorePointsAlert
             closeModal={closeModalHandler}
             points={1000}
