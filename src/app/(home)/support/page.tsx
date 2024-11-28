f98a99b77d02b4c2b7aa54f6f931ff689fb53703
Sergey-Markov <1nativefl@gmail.com>
2024-11-28 18:20:20 +0200

    add BackBtn to Support

--- src/app/(home)/support/page.tsx
+++ src/app/(home)/support/page.tsx
@@ -1,14 +1,22 @@
+import { useTranslations } from "next-intl";
+import Link from "next/link";
+import BackBtn from "@/components/BackBtn/BackBtn";
 import Contacts from "@/components/Contacts/Contacts";
 import { SUPPORT_CONTENT } from "@/constants";
-import { useTranslations } from "next-intl";
 
 const Support = () => {
   const t = useTranslations("Support");
+
   return (
     <main className=" min-h-screen p-4 md:p-8 text-white pb-24 font-extralight space-y-4 z-0">
-      <h2 className="text-primary text-4xl font-bold font-mono ">
-        {t("title")}
-      </h2>
+      <div className="mb-7 flex flex-row justify-between items-center">
+        <Link href="/">
+          <BackBtn />
+        </Link>
+        <h2 className="text-primary text-4xl font-mono font-bold">
+          {t("title")}
+        </h2>
+      </div>
       <p className="pl-1 ">{t("mainPageText")}</p>
       <ul className="space-y-4">
         {SUPPORT_CONTENT.map(({ id, title, text }) => {
