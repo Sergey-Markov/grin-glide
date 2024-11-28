01c738eaad52e8204d52da677b14e9919919d72c
Sergey-Markov <1nativefl@gmail.com>
2024-11-28 17:39:22 +0200

    add BackBtn

--- src/app/(home)/roadmap/page.tsx
+++ src/app/(home)/roadmap/page.tsx
@@ -1,13 +1,18 @@
 "use client";
 
+import BackBtn from "@/components/BackBtn/BackBtn";
 import { roadStages } from "@/constants";
 import { useTranslations } from "next-intl";
+import { useRouter } from "next/router";
 
 const completedStages = roadStages.filter(({ isComplete }) => isComplete);
 const unCompletedStages = roadStages.filter(({ isComplete }) => !isComplete);
 
 const RoadMap = () => {
   const t = useTranslations("RoadMap");
+  const router = useRouter();
+
+  const backHandler = () => router.back();
 
   return (
     <main
@@ -18,10 +23,13 @@ const RoadMap = () => {
     >
       <div className="hero-overlay bg-opacity-60" />
       <div>
-        <section className="p-4 pb-24 md:p-8 md:pb-24">
-          <h2 className="mb-5 text-5xl font-bold font-mono text-primary">
-            {t("title")}
-          </h2>
+        <section className="p-4 pb-24 md:p-8 md:pb-24 space-y-6">
+          <div className="mb-7 flex flex-row justify-between items-center">
+            <BackBtn onClick={backHandler} />
+            <h2 className="text-primary text-4xl font-mono font-bold">
+              {t("title")}
+            </h2>
+          </div>
           <ul className="steps steps-vertical">
             {completedStages.map(({ id, title }) => (
               <li
