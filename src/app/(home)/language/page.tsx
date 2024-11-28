ea3a67239f4683d88e2867ea3d0aaa516d77413b
Sergey-Markov <1nativefl@gmail.com>
2024-11-28 17:37:00 +0200

    add BackBtn

--- src/app/(home)/language/page.tsx
+++ src/app/(home)/language/page.tsx
@@ -10,10 +10,15 @@ import { localesBtns } from "@/constants";
 import { useTranslations } from "next-intl";
 import { updateUserFields } from "@/services/updateUserFields";
 import { useUser } from "@/app/contexts/AppContext";
+import { useRouter } from "next/router";
+import BackBtn from "@/components/BackBtn/BackBtn";
 
 const Language = () => {
   const { user, updateUser } = useUser();
   const t = useTranslations("LanguagePage");
+  const router = useRouter();
+
+  const backHandler = () => router.back();
 
   useEffect(() => {
     if (user?.selected_language) {
@@ -38,10 +43,13 @@ const Language = () => {
   };
 
   return (
-    <main className="min-h-screen p-4 md:p-8 text-white pb-24">
-      <h2 className="text-primary text-4xl font-bold font-mono mb-6">
-        {t("title")}
-      </h2>
+    <main className="min-h-screen p-4 md:p-8 text-white pb-24 space-y-6">
+      <div className="mb-7 flex flex-row justify-between items-center">
+        <BackBtn onClick={backHandler} />
+        <h2 className="text-primary text-4xl font-mono font-bold">
+          {t("title")}
+        </h2>
+      </div>
       <ul className="grid grid-cols-2 grid-rows-2 gap-1 sm:grid-cols-3 sm:grid-rows-3 sm:gap-2 md:grid-cols-5 md:grid-rows-5 md:gap-3">
         {localesBtns.map((btn) => {
           const isChecked = btn.value === user?.selected_language;
