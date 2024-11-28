52d709d73a25d35a272362f036f2016c63876cfa
Sergey-Markov <1nativefl@gmail.com>
2024-11-28 18:08:24 +0200

    change styles and translate of Language page

--- src/app/(home)/language/page.tsx
+++ src/app/(home)/language/page.tsx
@@ -41,11 +41,11 @@ const Language = () => {
 
   return (
     <main className="min-h-screen p-4 md:p-8 text-white pb-24 space-y-6">
-      <div className="mb-7 flex flex-row justify-between items-center">
+      <div className="flex flex-row justify-between items-center">
         <Link href="/">
           <BackBtn />
         </Link>
-        <h2 className="text-primary text-4xl font-mono font-bold">
+        <h2 className="text-primary text-3xl font-mono font-bold">
           {t("title")}
         </h2>
       </div>
