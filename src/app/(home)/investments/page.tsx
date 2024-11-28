733d45d99f0a1f2a0bde881788d2b5ee5a0564b9
Sergey-Markov <1nativefl@gmail.com>
2024-11-28 18:12:41 +0200

    try fix Error: NextRouter was not mounted

--- src/app/(home)/investments/page.tsx
+++ src/app/(home)/investments/page.tsx
@@ -1,20 +1,19 @@
 "use client";
 
+import Link from "next/link";
 import BackBtn from "@/components/BackBtn/BackBtn";
 import QuickBuyTokens from "@/components/QuickBuyTokens/QuickBuyTokens";
 import { useTranslations } from "next-intl";
-import { useRouter } from "next/router";
 
 const Investments = () => {
   const t = useTranslations("Investments");
-  const router = useRouter();
-
-  const backHandler = () => router.back();
 
   return (
     <main className=" min-h-screen p-4 md:p-8 text-white pb-24 space-y-6">
       <div className="mb-7 flex flex-row justify-between items-center">
-        <BackBtn onClick={backHandler} />
+        <Link href="/">
+          <BackBtn />
+        </Link>
         <h2 className="text-primary text-4xl font-mono font-bold">
           {t("title")}
         </h2>
