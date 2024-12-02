09736109315fdc9ea8da8ff86d339ce95a8b0e43
Sergey-Markov <1nativefl@gmail.com>
2024-12-02 13:30:01 +0200

    try check membership 7

--- src/services/checkMembership.ts
+++ src/services/checkMembership.ts
@@ -1,18 +1,20 @@
-// src/services/api.ts
+/* eslint-disable no-console */
 import axios from "axios";
 
-export async function checkChannelMembers(
-  telegramId: number,
-  channelUsername: string
-) {
-  const body = {
-    telegramId,
-    channelUsername,
-  };
+export const checkChannelMembers = async (
+  telegramId: string,
+  channelUserName: string
+) => {
   try {
-    const response = await axios.post("/api/check-membership", body);
-    return response;
-  } catch (error) {
-    throw new Error(`Failed to check, ${error}`);
+    const response = await axios.post("/api/checkMembership", {
+      telegramId,
+      channelUserName,
+    });
+    return response.data; // Або відповідно до API
+  } catch (error: any) {
+    console.error("Error in checkChannelMembers:", error);
+    throw new Error(
+      error.response?.data?.error || "Failed to check membership"
+    );
   }
-}
+};
