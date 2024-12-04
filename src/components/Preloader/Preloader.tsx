be3ad5e8a07004717db80745467dd184b0d38b40
Sergey-Markov <1nativefl@gmail.com>
2024-12-04 11:44:35 +0200

    try change color of loader & scale

--- src/components/Preloader/Preloader.tsx
+++ src/components/Preloader/Preloader.tsx
@@ -1,8 +1,8 @@
 const Preloader = () => (
   <main className="hero min-h-screen">
     <div className="hero-overlay bg-opacity-60" />
-    <div className="hero-content text-neutral-content text-center ">
-      <span className="loading loading-ring loading-lg" />
+    <div className="hero-content text-center ">
+      <span className="loading loading-ring loading-lg text-primary scale-50" />
     </div>
   </main>
 );
