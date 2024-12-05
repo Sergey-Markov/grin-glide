97c74b20f611646d9acc14433364d305203cd7f3
Sergey-Markov <1nativefl@gmail.com>
2024-12-05 20:47:51 +0200

    add conection to wallets

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 import React from "react";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import BtmNav from "@/components/BtmNav/BtmNav";
+
+const stringManifest =
+  "https://gold-ready-angelfish-861.mypinata.cloud/files/bafkreie5wx7s76uheyfbs4mniyssir3txqztqbpcce6uadnqesnm4isahq?X-Algorithm=PINATA1&X-Date=1733403628&X-Expires=30&X-Method=GET&X-Signature=b2b75a213c15caafa7dbe2821408749a0c597d6fa2b6aa89701fe3e2ab4cb9fe";
 
 export default function RootLayout({
   children,
@@ -9,9 +13,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
+    <TonConnectUIProvider manifestUrl={stringManifest}>
       {children}
       <BtmNav />
-    </>
+    </TonConnectUIProvider>
   );
 }
--- src/app/(home)/wallet/page.tsx
+++ src/app/(home)/wallet/page.tsx
@@ -1,12 +1,83 @@
+/* eslint-disable no-console */
+import { useCallback, useEffect, useState } from "react";
 import { useTranslations } from "next-intl";
-import Image from "next/image";
+import { useTonConnectUI } from "@tonconnect/ui-react";
+// import Address from "@tonconnect/sdk";
 import Link from "next/link";
-import { BiCheckDouble, BiWallet } from "react-icons/bi";
-import { walletsList } from "@/constants";
+// import Image from "next/image";
+// import { BiCheckDouble, BiWallet } from "react-icons/bi";
+// import { walletsList } from "@/constants";
 import BackBtn from "@/components/BackBtn/BackBtn";
+import Preloader from "@/components/Preloader/Preloader";
+import { BiWallet } from "react-icons/bi";
 
 const Wallet = () => {
+  const [tonConnectUI] = useTonConnectUI();
+  const [tonWalletAddress, setTonWalletAddress] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
   const t = useTranslations("Wallet");
+
+  const handleWalletConnection = useCallback((address: string) => {
+    setTonWalletAddress(address);
+    console.log("Wallet connected successfully");
+    setIsLoading(false);
+  }, []);
+
+  const handleWalletDisconnection = useCallback(() => {
+    setTonWalletAddress(null);
+    console.log("Wallet disconnected successfully");
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    const checkWalletConnection = async () => {
+      if (tonConnectUI.account?.address) {
+        handleWalletConnection(tonConnectUI.account?.address);
+      } else {
+        handleWalletDisconnection();
+      }
+    };
+    checkWalletConnection();
+
+    const unsubscribe = tonConnectUI.onStatusChange((wallet) => {
+      if (wallet) {
+        handleWalletConnection(wallet.account.address);
+      } else {
+        handleWalletDisconnection();
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [tonConnectUI, handleWalletConnection, handleWalletDisconnection]);
+
+  const handleWalletAction = async () => {
+    if (tonConnectUI.connected) {
+      setIsLoading(true);
+      await tonConnectUI.disconnect();
+    } else {
+      await tonConnectUI.openModal();
+    }
+  };
+
+  const formatAddress = (address: string) => {
+    const normalizeString = `${address.slice(0, 4)}...${address.slice(-4)}`;
+    return normalizeString;
+  };
+  // const tempAddress = Address.name(address);
+  // const tempAddress = Address.parse(address).toString();
+  // return `${tempAddress.slice(0, 4)}...${tempAddress.slice(-4)}`;
+
+  if (isLoading) {
+    return (
+      <main className="min-h-screen flex-col items-center justify-center">
+        <div className="mb-7">
+          <Preloader />
+        </div>
+      </main>
+    );
+  }
 
   return (
     <main className="min-h-screen p-4 md:p-8 text-white pb-24 space-y-6">
@@ -18,7 +89,22 @@ const Wallet = () => {
           {t("title")}
         </h2>
       </div>
-      <ul className="space-y-4">
+      <div className="flex items-center justify-between bg-emerald-700 rounded-full p-2 pr-4">
+        <BiWallet className="w-12 h-12 rounded-full mr-3 bg-teal-950 p-2" />
+        <p className="font-semibold">
+          {tonWalletAddress
+            ? formatAddress(tonWalletAddress)
+            : "Connect a wallet"}
+        </p>
+        <button
+          type="button"
+          onClick={handleWalletAction}
+        >
+          {tonWalletAddress ? "disconnect" : "connect"}
+        </button>
+      </div>
+
+      {/* <ul className="space-y-4">
         {walletsList.map(({ id, name, avatarSrc, isConnect }) => (
           <li
             key={id}
@@ -41,7 +127,7 @@ const Wallet = () => {
             {isConnect && <BiCheckDouble className="w-9 h-9 text-success " />}
           </li>
         ))}
-      </ul>
+      </ul> */}
     </main>
   );
 };
