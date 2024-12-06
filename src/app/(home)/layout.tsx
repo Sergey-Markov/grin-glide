a8e55a14ce1f1f90177ac041e8d4664d3bca303d
Sergey-Markov <1nativefl@gmail.com>
2024-12-06 12:20:35 +0200

    change url of manifest  with GrinGlideurl

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -5,7 +5,7 @@ import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import BtmNav from "@/components/BtmNav/BtmNav";
 
 const stringManifest =
-  "https://gold-ready-angelfish-861.mypinata.cloud/ipfs/bafkreievfyuvu6u5u3zx7skaamnk7e4nfqf2cltq7kikkshmcf464gploe";
+  "https://gold-ready-angelfish-861.mypinata.cloud/ipfs/bafkreiej674ff666xzihkfaw5t76342jwckobe5obtil67kqsnmvbsuw2q";
 
 export default function RootLayout({
   children,
