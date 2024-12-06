4a734607d3494782c72243c262de5ed3f1e143a6
Sergey-Markov <1nativefl@gmail.com>
2024-12-06 11:11:50 +0200

    try add TonConnectBtn

--- src/app/(home)/wallet/page.tsx
+++ src/app/(home)/wallet/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+/* eslint-disable no-unused-vars */
+
 /* eslint-disable no-console */
-import { useCallback, useEffect, useState } from "react";
+// import { useCallback, useEffect, useState } from "react";
 import { useTranslations } from "next-intl";
-import { useTonConnectUI } from "@tonconnect/ui-react";
+import { TonConnectButton, useTonConnectUI } from "@tonconnect/ui-react";
 // import Address from "@tonconnect/sdk";
 import Link from "next/link";
 // import Image from "next/image";
@@ -14,73 +16,72 @@ import Preloader from "@/components/Preloader/Preloader";
 import { BiWallet } from "react-icons/bi";
 
 const Wallet = () => {
-  const [tonConnectUI] = useTonConnectUI();
-  const [tonWalletAddress, setTonWalletAddress] = useState<string | null>(null);
-  const [isLoading, setIsLoading] = useState(true);
   const t = useTranslations("Wallet");
+  // const [tonConnectUI] = useTonConnectUI();
+  // const [tonWalletAddress, setTonWalletAddress] = useState<string | null>(null);
+  // const [isLoading, setIsLoading] = useState(true);
 
-  const handleWalletConnection = useCallback((address: string) => {
-    setTonWalletAddress(address);
-    console.log("Wallet connected successfully");
-    setIsLoading(false);
-  }, []);
+  // const handleWalletConnection = useCallback((address: string) => {
+  //   setTonWalletAddress(address);
+  //   console.log("Wallet connected successfully");
+  //   setIsLoading(false);
+  // }, []);
 
-  const handleWalletDisconnection = useCallback(() => {
-    setTonWalletAddress(null);
-    console.log("Wallet disconnected successfully");
-    setIsLoading(false);
-  }, []);
+  // const handleWalletDisconnection = useCallback(() => {
+  //   setTonWalletAddress(null);
+  //   console.log("Wallet disconnected successfully");
+  //   setIsLoading(false);
+  // }, []);
 
-  useEffect(() => {
-    const checkWalletConnection = async () => {
-      if (tonConnectUI.account?.address) {
-        handleWalletConnection(tonConnectUI.account?.address);
-      } else {
-        handleWalletDisconnection();
-      }
-    };
-    checkWalletConnection();
+  // useEffect(() => {
+  //   const checkWalletConnection = async () => {
+  //     if (tonConnectUI.account?.address) {
+  //       handleWalletConnection(tonConnectUI.account?.address);
+  //     } else {
+  //       handleWalletDisconnection();
+  //     }
+  //   };
+  //   checkWalletConnection();
 
-    const unsubscribe = tonConnectUI.onStatusChange((wallet) => {
-      if (wallet) {
-        handleWalletConnection(wallet.account.address);
-      } else {
-        handleWalletDisconnection();
-      }
-    });
+  //   const unsubscribe = tonConnectUI.onStatusChange((wallet) => {
+  //     if (wallet) {
+  //       handleWalletConnection(wallet.account.address);
+  //     } else {
+  //       handleWalletDisconnection();
+  //     }
+  //   });
 
-    return () => {
-      unsubscribe();
-    };
-  }, [tonConnectUI, handleWalletConnection, handleWalletDisconnection]);
+  //   return () => {
+  //     unsubscribe();
+  //   };
+  // }, [tonConnectUI, handleWalletConnection, handleWalletDisconnection]);
 
-  const handleWalletAction = async () => {
-    if (tonConnectUI.connected) {
-      setIsLoading(true);
-      await tonConnectUI.disconnect();
-    } else {
-      await tonConnectUI.openModal();
-    }
-  };
+  // const handleWalletAction = async () => {
+  //   if (tonConnectUI.connected) {
+  //     setIsLoading(true);
+  //     await tonConnectUI.disconnect();
+  //   } else {
+  //     await tonConnectUI.openModal();
+  //   }
+  // };
 
-  const formatAddress = (address: string) => {
-    const normalizeString = `${address.slice(0, 4)}...${address.slice(-4)}`;
-    return normalizeString;
-  };
+  // const formatAddress = (address: string) => {
+  //   const normalizeString = `${address.slice(0, 4)}...${address.slice(-4)}`;
+  //   return normalizeString;
+  // };
   // const tempAddress = Address.name(address);
   // const tempAddress = Address.parse(address).toString();
   // return `${tempAddress.slice(0, 4)}...${tempAddress.slice(-4)}`;
 
-  if (isLoading) {
-    return (
-      <main className="min-h-screen flex-col items-center justify-center">
-        <div className="mb-7">
-          <Preloader />
-        </div>
-      </main>
-    );
-  }
-
+  // if (isLoading) {
+  //   return (
+  //     <main className="min-h-screen flex-col items-center justify-center">
+  //       <div className="mb-7">
+  //         <Preloader />
+  //       </div>
+  //     </main>
+  //   );
+  // }
   return (
     <main className="min-h-screen p-4 md:p-8 text-white pb-24 space-y-6">
       <div className="mb-7 flex flex-row justify-between items-center">
@@ -93,17 +94,18 @@ const Wallet = () => {
       </div>
       <div className="flex items-center justify-between bg-emerald-700 rounded-full p-2 pr-4">
         <BiWallet className="w-12 h-12 rounded-full mr-3 bg-teal-950 p-2" />
-        <p className="font-semibold">
-          {tonWalletAddress
-            ? formatAddress(tonWalletAddress)
-            : "Connect a wallet"}
-        </p>
-        <button
-          type="button"
-          onClick={handleWalletAction}
-        >
-          {tonWalletAddress ? "disconnect" : "connect"}
-        </button>
+        {/* <p className="font-semibold">
+        {tonWalletAddress
+          ? formatAddress(tonWalletAddress)
+          : "Connect a wallet"}
+      </p>
+      <button
+        type="button"
+        onClick={handleWalletAction}
+      >
+        {tonWalletAddress ? "disconnect" : "connect"}
+      </button> */}
+        <TonConnectButton />
       </div>
 
       {/* <ul className="space-y-4">
@@ -133,5 +135,4 @@ const Wallet = () => {
     </main>
   );
 };
-
 export default Wallet;
