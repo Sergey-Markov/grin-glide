16e003fe8803d6e91799f0492771913d8534b1c5
Sergey-Markov <1nativefl@gmail.com>
2024-12-06 15:12:23 +0200

    look at full address of user wallet

--- src/app/(home)/wallet/page.tsx
+++ src/app/(home)/wallet/page.tsx
@@ -23,14 +23,11 @@ import { BiWallet } from "react-icons/bi";
 const Wallet = () => {
   const t = useTranslations("Wallet");
   const [tonConnectUI] = useTonConnectUI();
-  const walletInfo = useTonWallet();
-  const userFriendlyAddress = useTonAddress();
   const [tonWalletAddress, setTonWalletAddress] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(true);
 
   const currentWallet = tonConnectUI.wallet;
   const currentWalletInfo = tonConnectUI.connected;
-  console.log(currentWallet, currentWalletInfo);
 
   const handleWalletConnection = useCallback((address: string) => {
     setTonWalletAddress(address);
@@ -67,23 +64,6 @@ const Wallet = () => {
     };
   }, [tonConnectUI, handleWalletConnection, handleWalletDisconnection]);
 
-  // const handleWalletAction = async () => {
-  //   if (tonConnectUI.connected) {
-  //     setIsLoading(true);
-  //     await tonConnectUI.disconnect();
-  //   } else {
-  //     await tonConnectUI.openModal();
-  //   }
-  // };
-
-  // const formatAddress = (address: string) => {
-  //   const normalizeString = `${address.slice(0, 4)}...${address.slice(-4)}`;
-  //   return normalizeString;
-  // };
-  // const tempAddress = Address.name(address);
-  // const tempAddress = Address.parse(address).toString();
-  // return `${tempAddress.slice(0, 4)}...${tempAddress.slice(-4)}`;
-
   if (isLoading) {
     return (
       <main className="min-h-screen flex-col items-center justify-center">
@@ -93,10 +73,6 @@ const Wallet = () => {
       </main>
     );
   }
-  console.log("tonConnectUI:", tonConnectUI);
-  console.log("tonWalletAddress:", tonWalletAddress);
-  console.log("tonConnectUI.wallet:", tonConnectUI.wallet);
-  console.log("tonConnectUI.account?.address:", tonConnectUI.account?.address);
 
   return (
     <main className="min-h-screen p-4 md:p-8 text-white pb-24 space-y-6">
@@ -117,6 +93,7 @@ const Wallet = () => {
         )}
         <TonConnectButton />
       </div>
+      <p>{`full address: ${tonWalletAddress}`}</p>
     </main>
   );
 };
