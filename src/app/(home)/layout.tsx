772380b22b00d07a02fec9fef983251f07245969
Sergey-Markov <1nativefl@gmail.com>
2024-12-06 10:31:01 +0200

    change url of manifest

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -5,7 +5,7 @@ import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import BtmNav from "@/components/BtmNav/BtmNav";
 
 const stringManifest =
-  "https://gold-ready-angelfish-861.mypinata.cloud/files/bafkreie5wx7s76uheyfbs4mniyssir3txqztqbpcce6uadnqesnm4isahq?X-Algorithm=PINATA1&X-Date=1733403628&X-Expires=30&X-Method=GET&X-Signature=b2b75a213c15caafa7dbe2821408749a0c597d6fa2b6aa89701fe3e2ab4cb9fe";
+  "https://gold-ready-angelfish-861.mypinata.cloud/files/bafkreie5wx7s76uheyfbs4mniyssir3txqztqbpcce6uadnqesnm4isahq?X-Algorithm=PINATA1&X-Date=1733473637&X-Expires=30&X-Method=GET&X-Signature=057cda3e758eec4640cd67cce8fb7f7220b8881cccdddc36cb011689ff6eac17";
 
 export default function RootLayout({
   children,
