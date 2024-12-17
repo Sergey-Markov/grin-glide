5a094d5fe9116696fccd68349654b91ff7228e1d
Sergey-Markov <1nativefl@gmail.com>
2024-12-17 17:05:32 +0200

    add width

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -90,7 +90,7 @@ const HomePage = () => {
           {isCompletedVote ? (
             <section
               id="hero-home"
-              className="flex justify-center items-center mb-8"
+              className="flex justify-center items-center mb-8 w-screen  h-screen"
             >
               <LogoIcon />
             </section>
